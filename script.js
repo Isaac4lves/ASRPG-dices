19857debf4cf919bcd68f7ab3ec3b608748e0ae1
Isaac🎲 <cassioitavom@gmail.com>
2024-11-17 17:34:57 -0300

    Update script.html
    Mudando a indentação dos objetos dice6, dice10 e dice12.

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 let cache = [];
 const dice6 = {
     d1:"<img id='nada' src='source/Nada.png'>",
-    d1_txt:" Nada",
+    d1_txt:"[Nada]",
     d2:"<img id='nada' src='source/Nada.png'>",
-    d2_txt:" Nada",
+    d2_txt:"[Nada]",
     d3:"<img id='coruja' src='source/Coruja.png'>",
-    d3_txt:" Pressão",
+    d3_txt:"[Pressão]",
     d4:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d4_txt:"  Pressão e Adaptação",
+    d4_txt:"[Pressão e Adaptação]",
     d5:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d5_txt:"  Pressão e Adaptação",
+    d5_txt:"[Pressão e Adaptação]",
     d6:"<img id='joaninha' src='source/Joaninha.png'>",
-    d6_txt:" Sucesso"
+    d6_txt:"[Sucesso]"
 }
 
 const dice10 = {
     d1:"<img id='nada' src='source/Nada.png'>",
-    d1_txt:" Nada",
+    d1_txt:"[Nada]",
     d2:"<img id='nada' src='source/Nada.png'>",
-    d2_txt:" Nada",
+    d2_txt:"[Nada]",
     d3:"<img id='coruja' src='source/Coruja.png'>",
-    d3_txt:" Pressão",
+    d3_txt:"[Pressão]",
     d4:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d4_txt:"  Pressão e Adaptação",
+    d4_txt:"[Pressão e Adaptação]",
     d5:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d5_txt:"  Pressão e Adaptação",
+    d5_txt:"[Pressão e Adaptação]",
     d6:"<img id='joaninha' src='source/Joaninha.png'>",
-    d6_txt:" Sucesso",
+    d6_txt:"[Sucesso]",
     d7:"<img id='joaninha' src='source/Joaninha.png'> <img id='joaninha' src='source/Joaninha.png'>",
-    d7_txt:" 2x Sucessos",
+    d7_txt:"[2x Sucessos]",
     d8:"<img id='joaninha' src='source/Joaninha.png'> <img id='cervo' src='source/Cervo.png'>",
-    d8_txt:" Sucesso e Adptação",
+    d8_txt:"[Sucesso e Adptação]",
     d9:"<img id='joaninha' src='source/Joaninha.png'> <img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d9_txt:" Sucesso, Pressão e Adaptação",
+    d9_txt:"[Sucesso, Pressão e Adaptação]",
     d10:"<img id='coruja' src='source/Coruja.png'> <img id='coruja' src='source/Coruja.png'> <img id='coruja' src='source/Coruja.png'>",
-    d10_txt:" 3x Pressões"
+    d10_txt:"[3x Pressões]"
 }
 
 const dice12 = {
     d1:"<img id='nada' src='source/Nada.png'>",
-    d1_txt:" Nada",
+    d1_txt:"[Nada]",
     d2:"<img id='nada' src='source/Nada.png'>",
-    d2_txt:" Nada",
+    d2_txt:"[Nada]",
     d3:"<img id='coruja' src='source/Coruja.png'>",
-    d3_txt:" Pressão",
+    d3_txt:"[Pressão]",
     d4:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d4_txt:"  Pressão e Adaptação",
+    d4_txt:"[Pressão e Adaptação]",
     d5:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d5_txt:"  Pressão e Adaptação",
+    d5_txt:"[Pressão e Adaptação]",
     d6:"<img id='joaninha' src='source/Joaninha.png'>",
-    d6_txt:" Sucesso",
+    d6_txt:"[Sucesso]",
     d7:"<img id='joaninha' src='source/Joaninha.png'> <img id='joaninha' src='source/Joaninha.png'>",
-    d7_txt:" 2x Sucessos",
+    d7_txt:"[2x Sucessos]",
     d8:"<img id='joaninha' src='source/Joaninha.png'> <img id='cervo' src='source/Cervo.png'>",
-    d8_txt:" Sucesso e Adptação",
+    d8_txt:"[Sucesso e Adptação]",
     d9:"<img id='joaninha' src='source/Joaninha.png'> <img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'>",
-    d9_txt:" Sucesso, Pressão e Adaptação",
+    d9_txt:"[Sucesso, Pressão e Adaptação]",
     d10:"<img id='coruja' src='source/Coruja.png'> <img id='coruja' src='source/Coruja.png'> <img id='coruja' src='source/Coruja.png'>",
-    d10_txt:" 3x Pressões",
-    d11:"<img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'> <img id='cervo' src='source/Cervo.png'> <img id='coruja' src='source/Coruja.png'>",
-    d11_txt:" 2x Pressões 2x Adaptações",
+    d10_txt:"[3x Pressões]",
+    d11:"<img id='coruja' src='source/Coruja.png'>  <img id='coruja' src='source/Coruja.png'> <img id='cervo' src='source/Cervo.png'> <img id='cervo' src='source/Cervo.png'>",
+    d11_txt:"[2x Pressões 2x Adaptações]",
     d12:"<img id='coruja' src='source/Coruja.png'> <img id='coruja' src='source/Coruja.png'>",
-    d12_txt:" 2x Pressões"
+    d12_txt:"[2x Pressões]"
 }
 
 function clean(e){
@@ -71,7 +71,7 @@ function clean(e){
 }
 
 function showDice(src){
-    document.querySelector("#cache").innerHTML = cache;
+    document.querySelector("#cache").innerHTML = cache.join(' ');
     document.querySelector(".display").innerHTML =" ";
     document.querySelector(".display").innerHTML = src;
   
